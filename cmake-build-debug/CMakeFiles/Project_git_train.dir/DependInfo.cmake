86e2c6c075f821836f400f6ae3bda5b9c9d7e18c
moginski01 <moginski01@gmail.com>
2021-02-01 13:39:33 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Project_git_train.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/Mati/CLionProjects/Project_Git_train/main.cpp" "C:/Users/Mati/CLionProjects/Project_Git_train/cmake-build-debug/CMakeFiles/Project_git_train.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Project_git_train.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project_git_train.dir/main.cpp.obj"
+  "Project_git_train.exe"
+  "Project_git_train.exe.manifest"
+  "Project_git_train.pdb"
+  "libProject_git_train.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Project_git_train.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
